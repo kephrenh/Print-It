318674a74c93fafa3e469d512df2e479b6c0145f
Mohamed Amoussa <126504124+kephrenh@users.noreply.github.com>
2023-10-05 12:47:02 +0200

    Ajout des flèches

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,23 @@ const slides = [
 	// }
 ]
 
+const nbSlide = slides.length;
+const banner = document.getElementById("banner");
+let i = 0;
+
+// arrows
+function createArrows () {
+		let prevArrow = document.createElement("img");
+		prevArrow.classList.add("arrow", "arrow_left");
+		prevArrow.src = "assets/images/arrow_left.png";
+		prevArrow.alt = "Previous";
+		banner.appendChild(prevArrow);
+
+		let nextArrow = document.createElement("img");
+		nextArrow.classList.add("arrow", "arrow_right");
+		nextArrow.src = "assets/images/arrow_right.png";
+		nextArrow.alt = "Next";
+		banner.appendChild(nextArrow);
+}
+createArrows();
+
